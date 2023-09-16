42f19869c59729505b57e48e8811b0af4c998943
Benji250 <123209910+Benji250@users.noreply.github.com>
2023-09-16 10:19:50 -0700

    finished the activity

---
+++ python_setup.py
@@ -0,0 +1,13 @@
+def hello():
+    print("Hello rnadom person")
+
+def pack(clothes, toothbrush, snacks):
+    return(clothes, toothbrush, snacks)
+      
+hour = 3
+
+def eat_lunch():
+    if hour > 4:
+        print("First I Eat My Fruit ")
+    else hour = 3:
+        print("Next I Eat My Burrito")
